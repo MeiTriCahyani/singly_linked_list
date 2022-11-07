3463a7a9517fd681f70668d28a9be19ba3f697a8
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2022-11-07 18:10:11 +0700

    menambahkn Node

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -7,5 +7,15 @@ using System.Threading.Tasks;
 namespace singly_linked_list
 {
     //each node consist of the information part and link to the next node
-   
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+
+
+    }
+
+
+    
 }
