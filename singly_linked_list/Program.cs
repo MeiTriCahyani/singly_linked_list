8751072c061f1b7036fff60ce361dbb15d14bb9f
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2022-11-07 18:13:52 +0700

    menambahkan note

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -22,7 +22,53 @@ namespace singly_linked_list
         {
             START = null;
         }
+        public void addNote() //add a node in the list 
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\n Enter the roll number of the student:");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n Enter the roll name of the student");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+
+            // if the node to be inserted is the first node
+            if (START != null || rollNo <= START.rollNumber)
+            {
+                if ((START != null) && (rollNo <= START.rollNumber))
+                {
+                    Console.WriteLine();
+                    return;
+
+                }
+                newnode.next = START;
+                START = newnode;
+                return;
+
+            }
+
+            Node previous, current;
+            previous = START;
+            current = START;
+
+            while ((current != null) && (rollNo >= current.rollNumber))
+            {
+                if (rollNo == current.rollNumber)
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                previous.next = current;
+                previous.next = newnode;
+            }
+            newnode.next = current;
+            previous.next = newnode;
+        }
     }
+
+       
 
 
 }
