4ecbf4283c4023e2c828a7f88f5462becdd99ea8
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2022-11-07 17:30:03 +0700

    menambahkan bool search,

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -35,11 +35,70 @@ namespace singly_linked_list
             Node newnode = new Node();
             newnode.rollNumber = rollNo;
             newnode.name = nm;
-            if ((START != null) || (rollNo == START.rollNumber))
+
+            // if the node to be inserted is the first node
+            if (START != null || rollNo <= START.rollNumber)
             {
+                if((START != null) && ( rollNo <= START.rollNumber))
+                {
+                    Console.WriteLine();
+                    return;
+
+                }
+                newnode.next = START;
+                START = newnode;
+                return;
 
             }
+
+            Node previous, current;
+            previous = START;
+            current = START;
+
+            while((current != null) && (rollNo >= current.rollNumber))
+            {
+                if(rollNo == current.rollNumber)
+                {
+                    Console.WriteLine();
+                    return ;
+                }
+                previous.next = current;
+                previous.next = newnode;
+            }
+            newnode.next = current;
+            previous.next = newnode;
         }
+
+        public bool delNode(int rollNo)
+        {
+            Node previouse, current;
+            previouse = current = null;
+            if(Search(rollNo,ref previouse,ref current)==false)
+                return false;
+            previouse.next = current.next;
+            if(current == START)
+                START = START.next;
+            return true;
+        }
+
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            previous = START;
+            current = START;
+            while( (current != null) && ( rollNo != current.rollNumber))
+            {
+                previous = current;
+                current = current.next;
+
+            }
+            if (current == null)
+                return false;
+            else
+                return true;
+        }
+
+       
+
     }
 
 
